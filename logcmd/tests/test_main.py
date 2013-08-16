406226f8501340c5a56e8e23b110513f9a65ae8a
Nathan Wilcox <nejucomo@gmail.com>
2013-08-16 16:25:31 -0700

    Put top-level test code before utility code.

--- logcmd/tests/test_main.py
+++ logcmd/tests/test_main.py
@@ -9,6 +9,32 @@ from logcmd.tests.orderedsio import fake_select
 
 class MainTests (unittest.TestCase):
 
+    def test_default(self):
+        self._run_main_and_verify(
+            ['foo', 'bar'],
+            status=0,
+            expectedout="""\
+1970-01-01T00:00:00+0000 0 * Launched with args: ['foo', 'bar']
+1970-01-01T00:00:00+0000 0 - A
+1970-01-01T00:00:00+0000 0 ! B
+1970-01-01T00:00:00+0000 0 * Process exited with status: 0
+1970-01-01T00:00:00+0000 0 * Wall clock time: 0.000
+""",
+            )
+
+    def test_subcommand_parameter_collision(self):
+        self._run_main_and_verify(
+            ['foo', '-t', 'bar'],
+            status=0,
+            expectedout="""\
+1970-01-01T00:00:00+0000 0 * Launched with args: ['foo', '-t', 'bar']
+1970-01-01T00:00:00+0000 0 - A
+1970-01-01T00:00:00+0000 0 ! B
+1970-01-01T00:00:00+0000 0 * Process exited with status: 0
+1970-01-01T00:00:00+0000 0 * Wall clock time: 0.000
+""",
+            )
+
     def _run_main_and_verify(self, args, expectedout, status):
         sio = StringIO()
         fake_exit_calls = []
@@ -35,29 +61,3 @@ class MainTests (unittest.TestCase):
 
         self.assertEqual(expectedout, sio.getvalue())
         self.assertEqual([status], fake_exit_calls)
-
-    def test_default(self):
-        self._run_main_and_verify(
-            ['foo', 'bar'],
-            status=0,
-            expectedout="""\
-1970-01-01T00:00:00+0000 0 * Launched with args: ['foo', 'bar']
-1970-01-01T00:00:00+0000 0 - A
-1970-01-01T00:00:00+0000 0 ! B
-1970-01-01T00:00:00+0000 0 * Process exited with status: 0
-1970-01-01T00:00:00+0000 0 * Wall clock time: 0.000
-""",
-            )
-
-    def test_subcommand_parameter_collision(self):
-        self._run_main_and_verify(
-            ['foo', '-t', 'bar'],
-            status=0,
-            expectedout="""\
-1970-01-01T00:00:00+0000 0 * Launched with args: ['foo', '-t', 'bar']
-1970-01-01T00:00:00+0000 0 - A
-1970-01-01T00:00:00+0000 0 ! B
-1970-01-01T00:00:00+0000 0 * Process exited with status: 0
-1970-01-01T00:00:00+0000 0 * Wall clock time: 0.000
-""",
-            )
